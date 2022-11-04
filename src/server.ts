c6a47fb411766a62e6c381967bcdc9388d75fb42
Ivanilton Neto <netijua@gmail.com>
2022-11-04 17:11:09 -0300

    fix: Production configs

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,7 @@ import AppDataSource from "./data-source";
 import 'dotenv/config'
 
 (async () => {
-  const PORT = process.env.PORT;
-
+  
   await AppDataSource.initialize()
     .then(() => {
       console.log("Database connected");
@@ -13,7 +12,7 @@ import 'dotenv/config'
       console.error("Error during Data Source initialization", err);
     });
 
-  app.listen(process.env.PORT || PORT, () => {
-    console.log(`Serve running on port ${PORT}`);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Serve running`);
   });
 })();
