9ba9dd37ab639b07d7528639518663557b6b6809
Galvs135 <lg_franca@hotmail.com>
2022-11-04 00:09:37 -0300

    "feat: UpdateUser - created controller and service that will updating user data

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,12 @@
 import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
-import { IUserCreate } from "../interfaces";
+import { IUserCreate, IUserUpdateRequest } from "../interfaces";
 
 import createUserService from "../services/users/createUser.service";
 import deleteUserService from "../services/users/deleteUser.service";
 import listUserService from "../services/users/listUser.service";
 import searchUserService from "../services/users/searchUser.service";
+import updateUserService from "../services/users/updateUser.service";
 
 const createUserController = async (req: Request, res: Response) => {
   const user: IUserCreate = req.body;
@@ -13,22 +14,35 @@ const createUserController = async (req: Request, res: Response) => {
   return res.status(201).json(instanceToPlain(createdUser));
 };
 
-const deleteUserController = async (req: Request, res: Response) => {
-    const id: string = req.params.id;
-    await deleteUserService(id);
-    return res.status(200).json({ message: 'User deleted with success!' });
+const updateUserController = async (req: Request, res: Response) => {
+  const user: IUserUpdateRequest = req.body;
+  const id: string = req.params.id;
+  const token = req.users.id;
+  const updatedUser = await updateUserService(user, id, token);
+  return res.status(200).json(updatedUser);
 };
 
-const searchUserController =async (req: Request, res: Response) => {
-    const id: string = req.params.id
-    const user = await searchUserService(id)
-    return res.status(200).json(instanceToPlain(user))
-}
+const deleteUserController = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  await deleteUserService(id);
+  return res.status(200).json({ message: "User deleted with success!" });
+};
+
+const searchUserController = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  const user = await searchUserService(id);
+  return res.status(200).json(instanceToPlain(user));
+};
 
 const listUserController = async (req: Request, res: Response) => {
-    const users = await listUserService()
-    return res.status(200).json({users})
-}
+  const users = await listUserService();
+  return res.status(200).json({ users });
+};
 
-export  {createUserController, deleteUserController,listUserController, searchUserController} ;
-
+export {
+  createUserController,
+  deleteUserController,
+  listUserController,
+  searchUserController,
+  updateUserController,
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 
-import { createUserController, deleteUserController, listUserController, searchUserController } from "../controllers/users.controller";
+import {
+  createUserController,
+  deleteUserController,
+  listUserController,
+  searchUserController,
+  updateUserController,
+} from "../controllers/users.controller";
 import adminMiddleware from "../middlewares/admin.midleware";
 import { authMiddleware } from "../middlewares/auth.midleware";
 
@@ -9,8 +15,13 @@ const userRouter = Router();
 userRouter.post("", createUserController);
 userRouter.get("", authMiddleware, listUserController);
 userRouter.get("/:id", authMiddleware, searchUserController);
-userRouter.patch("/:id");
-userRouter.delete("/:id", authMiddleware, adminMiddleware, deleteUserController);
+userRouter.patch("/:id", authMiddleware, updateUserController);
+userRouter.delete(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  deleteUserController
+);
 userRouter.get("/:id/games");
 
 export default userRouter;
