67e2c533a0a26c7e9cce5024681d5d5ce103e424
luan-vieira <luan.vieir4@gmail.com>
2022-11-08 14:02:53 -0300

    feat-SearchUsers/Game

--- src/routes/games.routes.ts
+++ src/routes/games.routes.ts
@@ -1,16 +1,38 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { createGameController, deleteGameController, listGamesController, unsubscribeGameController, updateGameController,} from '../controllers/games.controller';
-import adminMiddleware from '../middlewares/admin.midleware';
-import { authMiddleware } from '../middlewares/auth.midleware';
+import {
+  createGameController,
+  deleteGameController,
+  listGamesController,
+  seachUsersGameController,
+  unsubscribeGameController,
+  updateGameController,
+} from "../controllers/games.controller";
+import adminMiddleware from "../middlewares/admin.midleware";
+import { authMiddleware } from "../middlewares/auth.midleware";
 
 const gamesRouter = Router();
 
-gamesRouter.post('', authMiddleware, adminMiddleware, createGameController);
-gamesRouter.get('', authMiddleware, listGamesController);
-gamesRouter.patch('/:id', authMiddleware, adminMiddleware, updateGameController);
-gamesRouter.patch('/unsubscribe/:id', authMiddleware, unsubscribeGameController);
-gamesRouter.delete('/:id', authMiddleware, adminMiddleware, deleteGameController);
-gamesRouter.get('/:id/games');
+gamesRouter.post("", createGameController);
+gamesRouter.get("", listGamesController);
+gamesRouter.patch(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  updateGameController
+);
+gamesRouter.patch(
+  "/unsubscribe/:id",
+  authMiddleware,
+  unsubscribeGameController
+);
+gamesRouter.delete(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  deleteGameController
+);
+gamesRouter.get("/:id/games");
+gamesRouter.get("/:id/users", seachUsersGameController);
 
 export default gamesRouter;
---
+++ src/services/Games/seachUsersGame.service.ts
@@ -0,0 +1,32 @@
+import AppDataSource from "../../data-source";
+import Games from "../../entities/games.intities";
+import UsersGames from "../../entities/usersGames.entities";
+import AppError from "../../errors/AppError";
+
+const seachUsersGameService = async (idGame: string): Promise<UsersGames[]> => {
+  const usersGameRepository = AppDataSource.getRepository(UsersGames);
+  const gamesRepository = AppDataSource.getRepository(Games);
+
+  const game = await gamesRepository.findOneBy({
+    id: idGame,
+  });
+
+  if (!game) {
+    throw new AppError("Game not found", 404);
+  }
+
+  const usersGame = await usersGameRepository.find({
+    where: {
+      games: {
+        id: game.id,
+      },
+    },
+    relations: {
+      users: true,
+    },
+  });
+
+  return usersGame;
+};
+
+export default seachUsersGameService;
